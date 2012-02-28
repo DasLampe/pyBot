b51e986ed2fbefdb00ecee9b0ba0e2c36961a7b0
DasLampe <daslampe@lano-crew.org>
2012-02-28 11:18:56 +0100

    Werner 2.0 (Beta)

--- bot.py
+++ bot.py
@@ -12,144 +12,19 @@
 ##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ##   GNU General Public License for more details.
 
-import xmpp, time, re, logging, mechanize
-from threading import Thread
-from random import randint
-
+import xmpp, time, re, mechanize,os
 DEBUG	= True
 
 def write_log(msg):
 	if DEBUG == True:
-		file		= open("/home/andre/pytalForumNotification/log.dat", "a")
-		file.write("["+time.asctime()+"]"+msg+"\n")
-		file.close()
-
-class Mucbot(Thread):
-	def __init__(self, jid, pwd, room, botname='', roompwd='', greeting=[], 
-			quotes=[], minwait=-1, maxwait=-1, reactions={}, delay=3, 
-			rcv_handler=[]):
-		'''initalize bot and let it join the room'''
-		Thread.__init__(self)
-		
-		if botname == '':
-			botname = jid.getNode()
-			
-		# the jid of the bot
-		self.jid = jid
-		# the room to join
-		self.room = room
-		# the nick to appear with
-		self.botname = botname
-		# the password for the room
-		self.roompwd = roompwd
-		# quotes to tell periodical
-		self.quotes = quotes
-		# minimum and maximum time to wait before quoting something
-		self.minwait = minwait
-		self.maxwait = maxwait
-		# a dictionary containing reactions. the keys have to be 
-		# regular expressions pattern as strings and the value a list of 
-		# reactions from which one will be picked randomly. it's sufficient to
-		# consider the lower case since every message body will be made lower
-		# case before parsingiit.
-		self.reactions = reactions
-		# seconds to wait before replying
-		self.delay = delay
-		# a list of handlers that will be executed when a message is received
-		self.rcv_handler = rcv_handler
-		# a list of possible greetings when the bot enters a room
-		self.greeting = greeting
-		
-		# compile the patterns
-		for key in self.reactions.keys():
-			self.reactions[re.compile(key)] = self.reactions.pop(key)
-
-		self.client = xmpp.Client(jid.getDomain(), debug=[])
-		self.client.connect()
-		self.client.RegisterHandler('message', self.msg_rcv)
-		self.client.RegisterHandler('presence', self.pres_rcv)
-		self.client.auth(jid.getNode(), pwd, resource=jid.getResource())
-		
-		self.join_room()
-
-	def join_room(self):
-		p = xmpp.Presence(to='%s/%s' % (self.room, self.botname))
-		p.setTag('x', namespace=xmpp.NS_MUC).setTagData('password', self.roompwd)
-		p.getTag('x').addChild('history', {'maxchars':'0', 'maxstanzas':'0'})
-		self.client.send(p)
-
-		self.jointime = time.time()
-
-		logging.info('%s joined %s' % (self.botname, self.room))
-
-	def msg_rcv(self, sess, msg):
-		'''will be executed when a message arrives'''
-
-		logging.debug('received message')
-
-		# ignore messages that come from this bot
-		sender = str(msg.getFrom())
-		if len(sender.split('/')) > 1:
-			sender = sender.split('/')[1]
-		if sender.lower().find(self.botname) >= 0:
-			return
-
-
-		# first execute all given handlers
-		for handler in self.rcv_handler:
-			handler(self, msg.getFrom(), msg.getBody())
-
-		self.react(msg.getBody())
-
-	def react(self, msg):
-		'''react to a message by searching for all patterns and replying with
-		the matching answer'''
-		logging.debug(msg)
-		msg = msg.lower()
-		time.sleep(self.delay)
-		for pattern in self.reactions.keys():
-			if pattern.search(msg):
-				self.say(self.reactions[pattern][randint(0,len(self.reactions[pattern])-1)])
-				return
-
-	def pres_rcv(self, sess, pres):
-		# ignore messages that come from this bot
-		sender = str(pres.getFrom())
-		if len(sender.split('/')) > 1:
-			sender = sender.split('/')[1]
-		if sender.lower().find(self.botname) >= 0:
-			return
-
-	def say(self, msg):
-		'''send the given message to the room'''
-		m = xmpp.Message(to=self.room, body=msg, typ='groupchat')
-		self.client.send(m)
-
-	def getClient(self):
-		return self.client	
-
-	def processing(self):
-		while True:
-			self.client.Process(1)
-
-	def run(self):
-		processor = Thread()
-		processor.run = self.processing
-		processor.start()
-		time.sleep(self.delay)
-		if len(self.greeting) > 0:
-			self.say(self.greeting[randint(0, len(self.greeting)-1)])
-		if self.minwait==-1 or self.maxwait==-1:
-			return
-		while True:
-			r = randint(self.minwait, self.maxwait)
-			logging.info('waiting %d seconds before next quote' % r)
-			time.sleep(r)
-			self.say(self.quotes[randint(0,len(self.quotes)-1)])
+		print("["+time.asctime()+"]"+msg)
+	file		= open(os.path.dirname(__file__)+"log.dat", "a")
+	file.write("["+time.asctime()+"]"+msg+"\n")
+	file.close()
 
 class pytalForum:
 	def __init__(self):
-		self.save_file				= "/home/andre/pytalForumNotification/pytalForum.dat"
+		self.save_file				= os.path.dirname(__file__)+"pytalForum.dat"
 		self.br						= mechanize.Browser()
 		self.br.addheaders			= [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/8.0')]
 		self.newest_post_username	= ""
@@ -231,27 +106,49 @@ class pytalForum:
 
 def main():
 	write_log("Start Bot")
-	jid = xmpp.JID('webmasterandre@jabber.pytal.net')
+	jid = xmpp.protocol.JID('webmasterandre@jabber.pytal.net')
 	pwd = '1Pf@df!nd3r'
 	room = 'pytal@conference.jabber.pytal.net'
-	examplebot = Mucbot(jid, pwd, room, 'pyBot Werner')
+	botname	= "pyBot Werner"
+	
+	write_log("Start Jabber Bot")
+	
+	bot	= xmpp.Client(jid.getDomain(), debug=[])
+	if bot.connect() == "":
+		write_log("Connection failed! Wait 5 min and try again.")
+		time.sleep(300)
+		main()
+		exit(0)
+
+	if bot.auth(jid.getNode(), pwd) == None:
+		write_log("Auth fail!")
+		exit(0)
+	
+	write_log("Join room")
+	join	= xmpp.protocol.Presence(to='%s/%s' % (room, botname))
+	bot.send(join)
+	
+	write_log("Jabber Bot successfuly starts")
 
 	forum   = pytalForum()
-
+	
+	write_log("End Init. Go into while")
 	while(1==1):
 		try:
 			posts   = forum.find_new_thread()
 			if posts is not False:
 				write_log("Send Message!")
 				write_log(posts)
-				examplebot.say(posts)
+				bot.send(xmpp.protocol.Message(to=room, body=posts, typ='groupchat'))
 				write_log("Message sends")
 			else:
 				write_log("Keine neuen Beiträge!")
+			write_log("Done ... wait 5 min and do again")
 			time.sleep(300)
 		except:
 			write_log("Error - Except!")
 			main()
+	bot.disconnect()
 	write_log("Close Bot")
 
 main()
